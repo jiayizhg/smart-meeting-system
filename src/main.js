f04843ceaf47d761520e3416defe15c81d53866a
jiayizhg <jiayi.zhang@knights.ucf.edu>
2023-10-18 18:17:26 -0400

    fix cors

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue/dist/vue.js'
 import App from './App.vue'
 import router from "./router"
 import './assets/styles/main.css'
+import Axios from 'axios'
+
 
 Vue.config.productionTip = false
+
+
+
+Vue.prototype.$axios = Axios
+Axios.defaults.baseURL = '/scheduling'
+
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
