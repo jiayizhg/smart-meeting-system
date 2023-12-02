80b591cf2c8748dcb1bbda9e89dac6417a6325cf
T <tinghuiwu@vt.edu>
2023-12-02 18:41:03 -0500

    new file:   src/components/RadarChart.vue

---
+++ src/components/RadarChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="chart-container">
+        <radar-chart :chart-data="chartData" :options="chartOptions"/>
+    </div>
+  </div>
+</template>
+<style>
+.chart-container {
+  min-width: 400px;
+}
+</style>
+<script>
+import { Radar } from 'vue-chartjs/legacy'
+
+export default {
+  extends: Radar,
+  props: ['chartData', 'options'],
+  async mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
